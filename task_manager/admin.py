d376f22c8fde6bc9ea07b192ab960b69326b7609
taras0024 <74822918+taras0024@users.noreply.github.com>
2021-08-16 11:36:08 +0300

    admin

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from task_manager.models import Tag, Task
+
+
+@admin.register(Tag)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created', 'updated',)
+
+
+@admin.register(Task)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created', 'updated', 'priority', 'status',)
